53996873d30b771ffbde63d7095fe751a31ed914
joao-marcelo-melo <marcelo25256464@gmail.com>
2023-10-12 19:42:49 -0300

    feat: create package utils

---
+++ src/utils/player_url.py
@@ -0,0 +1,13 @@
+from src.core.entities.players import Player
+
+players_urls = [
+    'https://tracker.gg/valorant/profile/riot/Zero%20m%C3%A1%20fase%238839/overview',
+    'bomdia'
+]
+
+# def mostrar():
+#     player = Player(players_urls)
+#     urls = player.return_players()
+
+#     for url in urls:
+#         print(url)
---
+++ src/utils/xpath.py
@@ -0,0 +1,10 @@
+class Player:
+    INPUT_PLAYER : str = '//*[@id="root"]/div/div[2]/div/div/div[1]/div/div/div/form/input'
+    PLAYER : str = '//*[@id="root"]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/div/div[2]/h3'
+
+class Status:
+    RANK : str = '/html/body/div/div[2]/div[3]/div/main/div[3]/div[3]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/span[2]'
+    ACCURACY_SHOT : str = '//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[3]/div[2]/div[2]/div[1]/div[1]/div[3]/div[3]/div/div[2]/span[2]'
+    HEADSHOT : str = '//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[3]/div[2]/div[2]/div[1]/div[1]/div[3]/div[1]/div/div[2]/span[2]'
+    LEVEL : str = '//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[3]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/span[2]'
+    KAST : str = '//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[3]/div[2]/div[2]/div[1]/div[1]/div[5]/div[2]/div/div[2]/span[2]'
